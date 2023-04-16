d31d99611c58f0527931c7b0c4b7bb0365832dc1
Ingsantirodriguez <santiago.rodriguez.98@mi.unc.edu.ar>
2023-04-16 17:14:55 -0300

    Capa superior (API Call) Finalizada

--- converter.c
+++ converter.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <curl/curl.h>
 #include "lib/cJSON.h"
+
+/**
+ * @brief Guarda en el archivo output.json la respuesta de la llamada a la API
+ *
+ * @param url de la llamada a la API
+ * @return 0 si funcionó de manera correcta o 1 si hubo un error
+ */
 
 int get_value(char *url)
 {
@@ -49,7 +57,15 @@ int get_value(char *url)
   return 1;
 }
 
-double read_output()
+/**
+ * @brief Lee el archivo "output.json" y devuelve el valor de la criptomoneda
+ * en $ARS.
+ *
+ * @param coin nombre de la moneda
+ * @return double valor de la criptomoneda en $ARS
+ */
+
+double read_output(char *coin)
 {
   double ars_value = 0;
   // Open the file for reading
@@ -57,7 +73,7 @@ double read_output()
   if (fp == NULL)
   {
     fprintf(stderr, "Error opening file\n");
-    return 1;
+    return 0;
   }
 
   // Read the contents of the file into a string
@@ -69,7 +85,7 @@ double read_output()
   {
     fprintf(stderr, "Error allocating memory\n");
     free(json_str);
-    return 1;
+    return 0;
   }
   fread(json_str, 1, file_size, fp);
   json_str[file_size] = '\0';
@@ -81,27 +97,19 @@ double read_output()
   {
     fprintf(stderr, "Error parsing JSON: %s\n", cJSON_GetErrorPtr());
     free(json_str);
-    return 1;
+    return 0;
   }
 
   // Extract the "ars" value from the JSON object
-  cJSON *ars_obj = cJSON_GetObjectItemCaseSensitive(cJSON_GetObjectItem(root, "bitcoin"), "ars");
+  cJSON *ars_obj = cJSON_GetObjectItemCaseSensitive(cJSON_GetObjectItem(root, coin), "ars");
   if (cJSON_IsNumber(ars_obj))
   {
     ars_value = ars_obj->valuedouble;
   }
   else
   {
-    cJSON *ars_obj = cJSON_GetObjectItemCaseSensitive(cJSON_GetObjectItem(root, "monero"), "ars");
-    if (cJSON_IsNumber(ars_obj))
-    {
-      ars_value = ars_obj->valuedouble;
-    }
-    else
-    {
-      fprintf(stderr, "Error getting \"ars\" value from JSON\n");
-      return 0;
-    }
+    fprintf(stderr, "Error getting \"ars\" value from JSON\n");
+    return 0;
   }
 
   // Clean up
@@ -115,23 +123,58 @@ int main(void)
 {
   char *url_bitcoin = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=ars";
   char *url_monero = "https://api.coingecko.com/api/v3/simple/price?ids=monero&vs_currencies=ars";
+  char *url_ethereum = "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=ars";
 
-  if (get_value(url_bitcoin))
-  {
-    return (1);
-  }
-  double bitcoin_in_ars = read_output(); // Valor en $ARS de un bitcoin
-  if (get_value(url_monero))
-  {
-    return (1);
-  }
-  double monero_in_ars = read_output(); // Valor en $ARS de un monero
+  int coin_id;
+  char coin_name[32];
+  double value_in_ars;
 
-  if (bitcoin_in_ars == 0 || monero_in_ars == 0)
+  while (1)
   {
-    return 1;
+    printf("Ingrese la criptomoneda:\n 1. Bitcoin\n 2. Monero\n 3. Ethereum\n");
+    scanf("%d", &coin_id);
+
+    switch (coin_id)
+    {
+    case 1:
+      if (get_value(url_bitcoin))
+      {
+        return (1);
+      }
+      value_in_ars = read_output("bitcoin"); // Valor en $ARS de un bitcoin
+      strcpy(coin_name, "BTC");
+      break;
+
+    case 2:
+      if (get_value(url_monero))
+      {
+        return (1);
+      }
+      value_in_ars = read_output("monero"); // Valor en $ARS de un monero
+      strcpy(coin_name, "XMR");
+      break;
+
+    case 3:
+      if (get_value(url_ethereum))
+      {
+        return (1);
+      }
+      value_in_ars = read_output("ethereum"); // Valor en $ARS de un ether
+      strcpy(coin_name, "ETH");
+      break;
+
+    default:
+      printf("El valor ingresado no corresponde con ninguna criptomoneda\n");
+      break;
+    }
+
+    if (value_in_ars == 0)
+    {
+      return 1;
+    }
+
+    printf("Valor de 1 %s en $ARS = %1.0f\n", coin_name, value_in_ars);
   }
 
-  printf("Valor de 1 BTC en $ARS = %1.0f\nValor de 1 XMR en $ARS = %1.0f\n", bitcoin_in_ars, monero_in_ars);
   return 0;
 }
